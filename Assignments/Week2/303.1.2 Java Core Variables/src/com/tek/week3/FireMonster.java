d82acb4293b53a141bc38ce1559f042f993eedce
Casey Maloney <tuj57107@temple.edu>
2023-02-13 11:04:23 -0500

    Practice assignment 303.6.1

---
+++ Assignments/Week2/303.1.2 Java Core Variables/src/com/tek/week3/FireMonster.java
@@ -0,0 +1,16 @@
+package com.tek.week3;
+
+public class FireMonster extends Monster {
+	
+	
+	public FireMonster(String string) {
+		// TODO Auto-generated constructor stub
+		
+		super(string); 
+	}
+
+	public String attack() { 
+		return("Attack with fire!"); 
+	}
+
+}
---
+++ Assignments/Week2/303.1.2 Java Core Variables/src/com/tek/week3/StoneMonster.java
@@ -0,0 +1,16 @@
+package com.tek.week3;
+
+public class StoneMonster extends Monster{
+	
+	
+	public StoneMonster(String string) {
+		// TODO Auto-generated constructor stub
+		
+		super(string); 
+	}
+	
+	public String attack() { 
+		return("Attack with stones!"); 
+	}
+
+}

fdd4b8dc87d44e88fdb7fb59ae973e5b8bdb140a
Casey Maloney <tuj57107@temple.edu>
2023-02-21 16:19:36 -0500

    SBA examples

--- ClassExamples/Week3/src/com/tek/SBA/Question2.java
+++ ClassExamples/Week3/src/com/tek/SBA/Question2.java
@@ -1,5 +1,7 @@
 package com.tek.SBA;
 
+import java.util.Arrays;
+
 public class Question2 {
 	
 	//0 based array and 1 based array 
@@ -11,7 +13,8 @@ public class Question2 {
 	
 	//give you a number for a size into a constant 
 	private static final int ARRAY_SIZE =5; 
-	int [] array; 
+	int [] array = {1,2,3,4,5,6}; 
+	int [] arr; 
 	
 	public static void main(String [] args ) { 
 		
@@ -24,11 +27,17 @@ public class Question2 {
 		
 		calculateAverage(avg); 
 		
-//		q2.setValue(4); //value is -1 need to change 
-//		q2.setValue(5); //print the first message setting value 
-//		q2.setValue(4); //print 2nd message because it already been changed 
-//		
+		System.out.println("================================");
 		
+		q2.setValue(4); //value is -1 need to change 
+		q2.setValue(5); //print the first message setting value 
+		q2.setValue(4); //print 2nd message because it already been changed 
+		
+		
+		System.out.println("================================");
+		q2.move(50, 3); //value is -1 so
+		q2.move(40, 3); //value is then changed and moved 
+		q2.move(20, 4); //value is 1 from previous code so its set to 
 		
 	}
 	
@@ -39,12 +48,10 @@ public class Question2 {
 				//however since we are one based we need 5 elements 1-5 - size of the array is size 6 
 				//ignore the 0th element of the array 
 				
-				int[] arr = new int[ARRAY_SIZE+1];
+				arr = new int[ARRAY_SIZE+1];
 				
 				//initialize this entire array to a value......-1 can us Arrays.fill(this.arr, 1)
-				for (Integer i: arr) { 
-					arr[i]= 1; 
-				}
+				Arrays.fill(this.arr, -1);
 				
 				System.out.println("A new array has been created");
 		
@@ -74,12 +81,12 @@ public class Question2 {
 		//if the value at array[index] is == -1 then print a message saying that you set the value  to 1 
 		//otherwise print a message saying that it was already changed 
 		
-		if (array [value] == -1) { 
-			array [value] = 1; 
-			System.out.println("Index " + value + "value is set to 1");
+		if (arr [value] == -1) { 
+			arr[value] = 1; 
+			System.out.println("Index " + value + " value is set to 1");
 		}
 		else { 
-			System.out.println("Index " + value + "value is already set");
+			System.out.println("Index " + value + " value is already set");
 		}
 	}
 	
@@ -87,6 +94,15 @@ public class Question2 {
 		//if the value at array[id] != 1 then change the value to 1 ???? 
 		//if the value at array[id] == 1 then print a message saying "value with id: {id} is moved to {value}" 
 		//otherwise print a message saying "value us already -1" 
+
+		if(array[id] != 1) { 
+			array[id] = 1; 
+			System.out.println("the value is set to 1"); 
+		}
+		else if (array[id] == 1 ) { 
+			System.out.println("value with id: " + id + " is move to " + value);
+		}
+		
 		
 		
 		

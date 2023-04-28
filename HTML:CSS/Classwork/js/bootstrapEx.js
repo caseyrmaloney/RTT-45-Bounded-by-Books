ff61ff1b616f287d808672500464563ab059d42d
Casey Maloney <77736692+caseyrmaloney@users.noreply.github.com>
2023-04-28 10:28:30 -0400

    Delete HTML:CSS directory

--- HTML:CSS/Classwork/js/bootstrapEx.js
+++
@@ -1,9 +0,0 @@
-
-
-console.log('this is a test script'); 
-
-console.log('"3==3 ' + ("3" == 3)); 
-console.log(' 3 == 3 ' + (3==3)); 
-console.log('"3" == "3" ' + ("3" == "3")); 
-console.log('"3" === 3 ' + ("3" === 3)); 
-
